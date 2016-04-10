a45f4be1d7674238a52a0761a1ed9bcd7b6352f5
Tomasz Komoszeski <tomasz.komoszeski@gmail.com>
2016-04-10 19:50:30 +0200

    KMP

---
+++ KMP/KMP/KMPNext.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KMP
+{
+    class KMPNext
+    {
+        string s;
+        int[] kMPNext; //tablica KMPnext;
+        int b;
+
+        public KMPNext(string s)
+        {
+            this.s = s;
+            kMPNext = new int[s.Length + 1];
+            kMPNext[0] = -1; //wartownik
+            b = -1;
+            for(int i = 1; i <= s.Length; i++)
+            {
+                while ((b > -1) && (s[b] != s[i-1]))
+                {
+                    b = kMPNext[b];
+                }
+                b++;
+                if ((i == s.Length) || (s[i] != s[b]))
+                    kMPNext[i] = b;
+                else kMPNext[i] = kMPNext[b];
+            }
+        }
+
+        public int[] KMP_Next
+        {
+            get
+            {
+                return kMPNext;
+            }
+        }
+             
+    }
+}
---
+++ KMP/KMP/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KMP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "ABBBCAASDWQABAABABAABBABBAB";
+            string p = "ABAABAB";
+            int pp = 0;
+            int b = 0;
+
+            Console.WriteLine(p);
+            Console.WriteLine(s);
+
+            KMPNext pKMPNext = new KMPNext(p);
+            int[] KMPNext = pKMPNext.KMP_Next;
+            
+            //foreach (int x in KMPNext)
+            //    Console.Write(" " + x);
+
+            for(int i = 0; i < s.Length; i++)
+            {
+                while ((b > -1) && (p[b] != s[i])) b = KMPNext[b];
+                if (++b == p.Length)
+                { //znalazłem wzorzec;
+                    while (pp < i - b + 1)
+                    {
+                        Console.Write(" ");
+                        pp++;
+                    }
+                    Console.Write("^");
+                    pp++;
+                    b = KMPNext[b];
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
